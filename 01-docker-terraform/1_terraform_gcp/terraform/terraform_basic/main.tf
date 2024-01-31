fc6d7efd556e41218bdcfe9b349acd925fe2951b
Dylan Lam <56712068+dlam23@users.noreply.github.com>
2024-01-31 01:51:06 -0400

    Submit Homework 01

--- 01-docker-terraform/1_terraform_gcp/terraform/terraform_basic/main.tf
+++ 01-docker-terraform/1_terraform_gcp/terraform/terraform_basic/main.tf
@@ -8,24 +8,24 @@ terraform {
 }
 
 provider "google" {
-# Credentials only needs to be set if you do not have the GOOGLE_APPLICATION_CREDENTIALS set
-#  credentials = 
-  project = "<Your Project ID>"
+  # Credentials only needs to be set if you do not have the GOOGLE_APPLICATION_CREDENTIALS set
+  #  credentials = 
+  project = "galvanic-crow-412709"
   region  = "us-central1"
 }
 
 
 
 resource "google_storage_bucket" "data-lake-bucket" {
-  name          = "<Your Unique Bucket Name>"
-  location      = "US"
+  name     = "galvanic-crow-412709-bucket"
+  location = "US"
 
   # Optional, but recommended settings:
-  storage_class = "STANDARD"
+  storage_class               = "STANDARD"
   uniform_bucket_level_access = true
 
   versioning {
-    enabled     = true
+    enabled = true
   }
 
   lifecycle_rule {
@@ -33,7 +33,7 @@ resource "google_storage_bucket" "data-lake-bucket" {
       type = "Delete"
     }
     condition {
-      age = 30  // days
+      age = 3 // days
     }
   }
 
@@ -41,8 +41,8 @@ resource "google_storage_bucket" "data-lake-bucket" {
 }
 
 
-resource "google_bigquery_dataset" "dataset" {
-  dataset_id = "<The Dataset Name You Want to Use>"
-  project    = "<Your Project ID>"
+resource "google_bigquery_dataset" "demo_dataset" {
+  dataset_id = "example_dataset"
+  project    = "galvanic-crow-412709"
   location   = "US"
 }
